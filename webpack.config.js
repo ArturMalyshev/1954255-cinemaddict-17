4da925c3fc643615daa707e4a1d92d13b23333f7
Artur Malyshev <42480216+ArturMalyshev@users.noreply.github.com>
2022-04-30 22:47:34 +0300

    Настройка Webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'),
+          to: path.resolve(__dirname, 'build'),
+        }
+      ],
+    }),
+  ],
+  devtool: 'source-map',
+}
