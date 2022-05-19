27595206fd3eb0d0504217c41166e2344b90d4e2
Artur Malyshev <42480216+ArturMalyshev@users.noreply.github.com>
2022-05-19 23:54:33 +0300

    Сделал открытие попапа по клику на фильм + его закрытие

--- src/mock/data.js
+++ src/mock/data.js
@@ -136,11 +136,11 @@ const filmData = {
   }
 };
 
-function getRandom(min, max) {
+const getRandom = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
+};
 
 const getFilmInformation = (filmMockData) => {
   const comments = [];
